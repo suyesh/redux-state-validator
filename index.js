0c43ea7aaf29071c864cc63296219221f0e4364c
Suyesh Bhandari <suyeshb@gmail.com>
2018-07-27 12:01:10 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import tv4 from 'tv4';
+
+function createMiddleWare(schema, valid) {
+	return ({ dispatch, getState }) => next => action => {
+		next(action);
+		const result = tv4.validateResult(getState(), schema);
+		if (!result.valid) {
+			console.log(
+				'%cState Validation Failed:',
+				'background: #e74c3c; color: white',
+				{
+					[result.error.dataPath.split('/')[1]]: {
+						message: result.error.message,
+						validation: result.error.params
+					}
+				}
+			);
+		} else {
+			if (valid) {
+				return console.info(
+					'%cState Validation Passing',
+					'background: #27ae60; color: white'
+				);
+			}
+		}
+	};
+}
+
+const ReduxJsonSchema = createMiddleWare();
+ReduxJsonSchema.Schema = createMiddleWare;
+
+export default ReduxJsonSchema;
